d740cd5ad65f732310efe85dc3448ee59c2e6a5c
Younghoo Kim <sensitive.hu@gmail.com>
2023-10-25 01:02:57 +0900

    feat: 포토 카드 업로더 UI 구현

---
+++ src/components/PhotoCardUploader/index.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { useRef, useState } from "react";
+
+const PhotoCardUploader = () => {
+  const [file, setFile] = useState<File | undefined>();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const url = file && URL.createObjectURL(file);
+
+  return (
+    <div className="bg-[url(/icons/image-upload.svg)] bg-[center_193px] bg-no-repeat flex justify-center items-center w-[294px] h-[468px] rounded-[8px] border-[2px] border-solid divide-gray-01 relative">
+      {url ? (
+        <Image
+          src={url}
+          fill
+          objectFit="contain"
+          alt="유저가 포토 카드에 올린 이미지"
+        />
+      ) : (
+        <>
+          <label
+            htmlFor="image-uploader"
+            className="text-center text-body5 text-grey-02 mt-[110px] cursor-pointer"
+          >
+            이미지를 추가해
+            <br />
+            나만의 티켓을 만들어보세요.
+          </label>
+          <input
+            id="image-uploader"
+            ref={inputRef}
+            accept="image/*"
+            type="file"
+            hidden
+            onChange={(e) => {
+              const files = e.target.files;
+
+              if (files) {
+                setFile(files[0]);
+              }
+            }}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default PhotoCardUploader;
