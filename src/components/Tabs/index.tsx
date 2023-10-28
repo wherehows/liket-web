cf60a8835ab01ee8ed8b60091d91f4ab5ce3a268
Younghoo Kim <sensitive.hu@gmail.com>
2023-10-28 16:14:31 +0900

    refactor: classNames 헬퍼 함수 적용

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,5 +1,6 @@
 import { TabsContext, useTabsContext } from "@/hooks/useTabsContext";
 import { EmptyFunction, StrictPropsWithChildren } from "@/types/common";
+import { classNames } from "@/utils/helpers";
 import { useState } from "react";
 
 const Tabs = ({ children }: StrictPropsWithChildren) => {
@@ -15,9 +16,7 @@ const Tabs = ({ children }: StrictPropsWithChildren) => {
 
   return (
     <TabsContext.Provider value={providerValue}>
-      <ul className="w-[100%] h-[98px] flex pl-[24px] pr-[24px] pb-[72px] fixed bottom-0 shadow-[0px_-8px_16px_0px_rgba(0,0,0,0.04)]">
-        {children}
-      </ul>
+      {children}
     </TabsContext.Provider>
   );
 };
@@ -29,29 +28,36 @@ type TextTabProps = StrictPropsWithChildren<
   string
 >;
 
+const TabList = ({ children }: StrictPropsWithChildren) => {
+  return (
+    <ul className="w-[100%] h-[98px] flex pl-[24px] pr-[24px] pb-[72px] fixed bottom-0 shadow-[0px_-8px_16px_0px_rgba(0,0,0,0.04)]">
+      {children}
+    </ul>
+  );
+};
+
 const Tab = ({ children, onClickTab }: TextTabProps) => {
   const { selectedTab, setSelectedTab } = useTabsContext();
 
-  const buttonClass =
-    selectedTab === children
-      ? "text-button3 text-skyblue-01"
-      : "text-button4 text-grey-02";
-
-  const listClass =
-    selectedTab === children
-      ? "border-t-[2px] border-skyblue-01"
-      : "border-t-[1px] border-grey-01";
-
   return (
     <li
-      className={`w-[114px] h-[26px] flex items-center justify-center flex-1 border-solid pt-[8px] ${listClass}`}
+      className={classNames(
+        `w-[114px] h-[26px] flex items-center justify-center flex-1 border-solid pt-[8px]`,
+        selectedTab === children
+          ? "border-t-[2px] border-skyblue-01"
+          : "border-t-[1px] border-grey-01"
+      )}
     >
       <button
         onClick={() => {
           setSelectedTab(children);
           onClickTab();
         }}
-        className={buttonClass}
+        className={classNames(
+          selectedTab === children
+            ? "text-button3 text-skyblue-01"
+            : "text-button4 text-grey-02"
+        )}
       >
         {children}
       </button>
@@ -59,6 +65,7 @@ const Tab = ({ children, onClickTab }: TextTabProps) => {
   );
 };
 
+Tabs.TabList = TabList;
 Tabs.Tab = Tab;
 
 export default Tabs;
