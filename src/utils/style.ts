fe6bba5297ef3f99e82b5432fa64bb058550fdca
Younghoo Kim <sensitive.hu@gmail.com>
2023-11-12 18:22:53 +0900

    chore: 컬러 토큰추가

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -94,10 +94,12 @@ const ButtonScale: FontSize = {
 
 const colors = {
   grey: {
+    black: "rgba(0, 0, 0, 1)",
     "01": "rgba(240, 240, 240, 1)",
     "02": "rgba(217, 217, 217, 1)",
     "03": "rgba(176, 176, 176, 1)",
     "04": "rgba(124, 124, 124, 1)",
+    white: "rgba(255, 255, 255, 1)",
   },
   skyblue: {
     "01": "#00C2FF",
@@ -107,6 +109,12 @@ const colors = {
   rosepink: {
     "01": "#FF005C",
   },
+  red: "#e0483f",
+  orange: "#ec682c",
+  yellow: "#f5d949",
+  green: "#4eab87",
+  blue: "#3b85ca",
+  purple: "#45098c",
 };
 
 export type TypographyVariant = keyof typeof TypographyScale;
