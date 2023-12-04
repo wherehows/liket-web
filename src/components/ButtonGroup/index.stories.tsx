7a2e99fb1b03c4aa2bd98b536dfbe111e5b4e402
Younghoo Kim <sensitive.hu@gmail.com>
2023-12-04 23:12:30 +0900

    feat(component): 버튼그룹 생성

---
+++ src/components/ButtonGroup/index.stories.tsx
@@ -0,0 +1,35 @@
+import ButtonGroup from ".";
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "../Button";
+
+const meta: Meta<typeof ButtonGroup> = {
+  title: "ButtonGroup",
+  component: ButtonGroup,
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+
+type Index = StoryObj<typeof ButtonGroup>;
+
+export const Index: Index = {
+  render: ({ gap }) => {
+    return (
+      <div className="w-[400px]">
+        <ButtonGroup gap={gap}>
+          <Button fullWidth variant="ghost" height={40} onClick={() => {}}>
+            취소
+          </Button>
+          <Button fullWidth height={40} onClick={() => {}}>
+            확인
+          </Button>
+        </ButtonGroup>
+      </div>
+    );
+  },
+  args: {
+    gap: 8,
+  },
+};
