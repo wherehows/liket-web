3541fa073d8c78e1690a2e599c4b934b8bf0758e
wherehows <sensitive.hu@gmail.com>
2023-12-08 15:22:49 +0900

    chore(component): MiddleText 컴포넌트 textSize 받지 않도록 수정 및 h1 태그 적용

--- src/components/Header/MiddleText.tsx
+++ src/components/Header/MiddleText.tsx
@@ -1,15 +1,14 @@
 interface MiddleTextProps {
   text: string;
-  textSize?: number;
 }
 
-const MiddleText = ({ text, textSize = 16 }: MiddleTextProps) => {
+const MiddleText = ({ text }: MiddleTextProps) => {
   return (
-    <div
-      className={`absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-[${textSize}px]`}
+    <h1
+      className={`absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]`}
     >
       {text}
-    </div>
+    </h1>
   );
 };
 
