f06d6fdafff763b4d912c09d3cf6f0b8ce6093fb
wherehows <sensitive.hu@gmail.com>
2023-12-12 13:55:36 +0900

    feat(component): modal 구현 및 storybook 추가

---
+++ src/components/Modal/index.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta } from "@storybook/react";
+
+import Modal from ".";
+import useModalStore from "./store";
+import ModalProvider from "./ModalProvider";
+import Button from "../Button";
+import ButtonGroup from "../ButtonGroup";
+
+const meta: Meta<typeof Modal> = {
+  title: "Modal",
+  component: Modal,
+};
+
+export default meta;
+
+export const Index = {
+  render: function Render() {
+    const openModal = useModalStore(({ openModal }) => openModal);
+
+    return (
+      <ModalProvider>
+        <div className="w-[600px]">
+          <ButtonGroup gap={16}>
+            <Button
+              fullWidth
+              variant="ghost"
+              height={48}
+              onClick={() => {
+                openModal("DeleteCommentModal");
+              }}
+            >
+              삭제 모달 보기
+            </Button>
+            <Button
+              fullWidth
+              variant="ghost"
+              height={48}
+              onClick={() => {
+                openModal("LoginModal");
+              }}
+            >
+              로그인 모달 보기
+            </Button>
+          </ButtonGroup>
+        </div>
+      </ModalProvider>
+    );
+  },
+};
