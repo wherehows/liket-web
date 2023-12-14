a786f1b21a46020082d13795f857dda418e1369d
Younghoo Kim <sensitive.hu@gmail.com>
2023-12-14 18:10:03 +0900

    feat(storybook): 케러셀 추가

---
+++ src/components/Carousel/Carousel.stories.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Carousel from ".";
+
+const meta = {
+  title: "Carousel",
+};
+
+export default meta;
+
+export const Index = {
+  render: function Render() {
+    const imgs = [
+      "https://picsum.photos/id/237/390/280",
+      "https://picsum.photos/id/238/390/280",
+      "https://picsum.photos/id/239/390/280",
+      "https://picsum.photos/id/240/390/280",
+    ];
+
+    return (
+      <div className="w-[390px]">
+        <Carousel imgs={imgs} />
+      </div>
+    );
+  },
+};
