1d3323614961b1819a8f1eefea9afbdba53ac76b
Younghoo Kim <sensitive.hu@gmail.com>
2023-12-14 19:55:04 +0900

    chore(config): html lnag을 ko로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={appleGothic.className}>{children}</body>
     </html>
   );
