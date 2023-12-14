dd553a79c78ffa5b4624b5177a04e72dec93eaa5
Younghoo Kim <sensitive.hu@gmail.com>
2023-12-14 18:10:33 +0900

    chore(utils): getRefValue 함수 추가

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,10 @@
+import { RefObject } from "react";
+
 export const classNames = (...classes: (boolean | string)[]) =>
   classes.filter(Boolean).join(" ");
 
 export const getKeys = Object.keys as <T extends object>(
   obj: T
 ) => Array<keyof T>;
+
+export const getRefValue = <C>(ref: RefObject<C>) => ref.current as C;
