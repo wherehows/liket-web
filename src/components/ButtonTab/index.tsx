78acda7d229da6d6e797dbeff74db41fc7f2223f
wherehows <sensitive.hu@gmail.com>
2023-12-16 16:37:44 +0900

    chore(ButtonTab): gap prop 추가

--- src/components/ButtonTab/index.tsx
+++ src/components/ButtonTab/index.tsx
@@ -1,13 +1,16 @@
 import { StrictPropsWithChildren } from "@/types/common";
 import { classNames } from "@/utils/helpers";
 
-type ButtonTab = StrictPropsWithChildren<{ shadow?: boolean }>;
+type ButtonTab = StrictPropsWithChildren<{ shadow?: boolean; gap?: string }>;
 
-const ButtonTab = ({ children, shadow = false }: ButtonTab) => {
+const ButtonTab = ({ children, shadow = false, gap }: ButtonTab) => {
   return (
     <div
+      style={{
+        gap,
+      }}
       className={classNames(
-        `bottom-tab pt-[8px] pl-[24px] pr-[24px] pb-[42px]`,
+        `bottom-tab flex flex-row pt-[8px] pl-[24px] pr-[24px] pb-[42px]`,
         shadow && `shadow-[0px_-8px_16px_0px_rgba(0,0,0,0.04)]`
       )}
     >
