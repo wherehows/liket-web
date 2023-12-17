26f6791ead160111e7f52c0b894d36908ff330c3
Younghoo Kim <sensitive.hu@gmail.com>
2023-12-18 07:15:39 +0900

    feat(Map): 맵 페이지에 맵 삽입

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,14 +1,15 @@
-import Carousel from "@/components/Carousel";
 import Header from "@/components/Header";
 import LinkableTab from "@/components/LinkableTab";
+import Map from "@/components/Map";
 
-export default function Map() {
+export default function MapPage() {
   return (
     <>
       <Header>
         <Header.LeftOption townSelection />
         <Header.RightOption option={{ search: true, like: true }} />
       </Header>
+      <Map width="100%" height="calc(100% - 48px - 74px)" />
       <LinkableTab />
     </>
   );
