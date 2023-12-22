115acc9dc1babbdf51f1e7d522a987e5983a5e7a
wherehows <sensitive.hu@gmail.com>
2023-12-22 16:27:41 +0900

    style(Map): 스토리북 내 불필요한 wrapper 스타일링 제거

--- src/components/Pages/Map.stories.tsx
+++ src/components/Pages/Map.stories.tsx
@@ -11,14 +11,14 @@ export default meta;
 export const Map = {
   render: () => {
     return (
-      <div className="max-w-[490px] h-[100%] mx-[auto]">
+      <>
         <Header>
           <Header.LeftOption townSelection />
           <Header.RightOption option={{ search: true, like: true }} />
         </Header>
         <KaKaoMap width="100%" height="calc(100% - 48px - 74px)" />
         <LinkableTab />
-      </div>
+      </>
     );
   },
 };
