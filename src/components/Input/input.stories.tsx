c126a4442fdd92a110d260c251c7993a9626633a
wherehows <sensitive.hu@gmail.com>
2024-01-09 13:43:13 +0900

    chore(Input): Button 요소에서 isShown 제거

--- src/components/Input/input.stories.tsx
+++ src/components/Input/input.stories.tsx
@@ -42,7 +42,7 @@ export const Index = {
             <Input margin="0 0 30px 0">
               <Input.Label htmlFor="email">이메일</Input.Label>
               <Input.Content id="email" placeholder="이메일 입력" />
-              <Input.Button isShown disabled={true} onClick={() => {}}>
+              <Input.Button disabled={true} onClick={() => {}}>
                 인증받기
               </Input.Button>
               <Input.Text isShown={false}>03분 00초</Input.Text>
@@ -58,7 +58,7 @@ export const Index = {
             <Input margin="0 0 30px 0">
               <Input.Label htmlFor="password-check">이메일</Input.Label>
               <Input.Content id="password-check" placeholder="이메일 입력" />
-              <Input.Button isShown disabled={false} onClick={() => {}}>
+              <Input.Button disabled={false} onClick={() => {}}>
                 인증받기
               </Input.Button>
               <Input.Text isShown={false}>03분 00초</Input.Text>
@@ -66,10 +66,10 @@ export const Index = {
             <Input margin="0 0 30px 0">
               <Input.Label htmlFor="check-number">인증 번호 확인</Input.Label>
               <Input.Content id="check-number" placeholder="인증번호 확인" />
-              <Input.Button isShown={false} disabled={false} onClick={() => {}}>
+              <Input.Button disabled={false} onClick={() => {}}>
                 인증받기
               </Input.Button>
-              <Input.Text isShown={true}>03분 00초</Input.Text>
+              <Input.Text isShown={false}>03분 00초</Input.Text>
             </Input>
           </form>
         </FormProvider>
