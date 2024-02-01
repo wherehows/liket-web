919020e4ef53bd7649c778ab7c027f9e59bf297f
wherehows <sensitive.hu@gmail.com>
2024-02-01 22:57:43 +0900

    feat(HotPlaceItem): 컴포넌트 구현 및 적용

---
+++ src/components/List/index.stories.tsx
@@ -0,0 +1,17 @@
+import HotPlaceItem, { HOT_PLACE_DUMMY_1 } from "./Hotplace";
+
+export default {
+  title: "components/List",
+};
+
+export const HotPlaceIndex = {
+  render: () => {
+    return (
+      <>
+        {HOT_PLACE_DUMMY_1.map((data) => (
+          <HotPlaceItem {...data} />
+        ))}
+      </>
+    );
+  },
+};
