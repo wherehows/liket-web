9412bace55607205f12048db3ef8f6163fef7717
wherehows <sensitive.hu@gmail.com>
2024-02-09 20:31:49 +0900

    feat(Chip): Chip 컴포넌트 생성

---
+++ src/components/Chip/index.stories.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { MouseEvent } from "react";
+import Chip from ".";
+import { GENRES, GENRESType } from "@/utils/const";
+
+export default {
+  title: "components/Chip",
+};
+
+const GENDER = ["남자", "여자"] as const;
+
+export const SingleSelectionChips = {
+  render: function Render() {
+    const [selectedGender, setSelcetedGender] = useState<
+      (typeof GENDER)[number]
+    >(GENDER[0]);
+    const onClickChip = (e: MouseEvent<HTMLUListElement>) => {
+      const target = e.target as HTMLUListElement;
+
+      if (target.tagName === "BUTTON") {
+        setSelcetedGender(target.textContent as (typeof GENDER)[number]);
+      }
+    };
+
+    return (
+      <ul
+        className="flex flex-wrap gap-[8px] w-[100%]"
+        onClick={(e) => onClickChip(e)}
+      >
+        {GENDER.map((gender) => {
+          return (
+            <li key={gender}>
+              <Chip isSelected={selectedGender === gender}>{gender}</Chip>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  },
+};
+
+export const MultipleSelectionChips = {
+  render: function Render() {
+    const [selectedGenres, setSelectedGenres] = useState<
+      (typeof GENRES)[number][]
+    >([]);
+    const onClickChip = (e: MouseEvent<HTMLUListElement>) => {
+      const target = e.target as HTMLUListElement;
+
+      if (target.tagName === "BUTTON") {
+        setSelectedGenres([
+          ...selectedGenres,
+          target.textContent as (typeof GENRES)[number],
+        ]);
+      }
+    };
+
+    return (
+      <ul
+        className="flex flex-wrap gap-[8px] w-[100%]"
+        onClick={(e) => onClickChip(e)}
+      >
+        {GENRES.map((genre: (typeof GENRES)[number]) => {
+          return (
+            <li key={genre}>
+              <Chip isSelected={selectedGenres.includes(genre)}>{genre}</Chip>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  },
+};
---
+++ src/components/Chip/index.tsx
@@ -0,0 +1,25 @@
+import { StrictPropsWithChildren } from "@/types/common";
+import { classNames } from "@/utils/helpers";
+
+type ChipProps = StrictPropsWithChildren<
+  {
+    isSelected: boolean;
+  },
+  string
+>;
+
+const Chip = ({ children, isSelected }: ChipProps) => {
+  return (
+    <button
+      className={classNames(
+        "text-button4 rounded-[12px] border-solid border-[1px] px-[8px] h-[24px] center",
+        isSelected && "text-white bg-skyblue-01 border-skyblue-01"
+      )}
+      aria-selected={isSelected}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Chip;
