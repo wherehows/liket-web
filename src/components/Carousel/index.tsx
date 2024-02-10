93d36a5cb09d319fc174001e52b192516f07c064
wherehows <sensitive.hu@gmail.com>
2024-02-10 21:25:31 +0900

    feat(Control): 컴포넌트 구현

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -5,6 +5,7 @@ import { getTouchEventData } from "@/utils/dom";
 import { classNames, getRefValue } from "@/utils/helpers";
 import Image from "next/image";
 import { useRef, useState } from "react";
+import Control from "../Control";
 
 const MIN_SWIPE_REQUIRED = 40;
 
@@ -144,13 +145,9 @@ const Carousel = ({ imgs }: CarouselProps) => {
         >
           {imgs.map((_, index) => {
             return (
-              <button
-                key={index}
+              <Control
                 onClick={() => handleClickControl(index)}
-                className={classNames(
-                  "w-[8px] h-[8px] rounded-[50%]",
-                  currentIndex === index ? "bg-skyblue-01" : "bg-grey-01"
-                )}
+                isSelected={currentIndex === index}
               />
             );
           })}
