09f52343f04128022b5d8206a2322d1a3eb5d965
wherehows <sensitive.hu@gmail.com>
2024-02-12 18:41:18 +0900

    feat(AvatarUploader): 아바타 업로더 구현

---
+++ src/components/AvatarUploader/index.stories.tsx
@@ -0,0 +1,9 @@
+import AvatarUploader from ".";
+
+export default {
+  title: "components/AvatarUploader",
+};
+
+export const Index = {
+  render: () => <AvatarUploader />,
+};
--- src/components/AvatarUploader/index.tsx
+++ src/components/AvatarUploader/index.tsx
@@ -1,5 +1,50 @@
+import { getRefValue } from "@/utils/helpers";
+import Image from "next/image";
+import { useRef, useState } from "react";
+
 const AvatarUploader = () => {
-  return <div></div>;
+  const [file, setFile] = useState<File | null>(null);
+  const imageUrl = file && URL.createObjectURL(file);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className="w-[80px] h-[80px] relative">
+      <div className="w-[80px] h-[80px] rounded-full relative overflow-hidden">
+        <Image
+          src={imageUrl ? imageUrl : "/icons/default-avatar.svg"}
+          alt="아바타 이미지"
+          layout="fill"
+          fill
+          objectFit="cover"
+        />
+      </div>
+      <input
+        ref={inputRef}
+        type="file"
+        className="hidden"
+        onChange={(e) => {
+          const files = e.target.files;
+
+          if (files) {
+            setFile(files[0]);
+          }
+        }}
+      />
+      <button
+        className="absolute right-0 bottom-0"
+        onClick={() => {
+          getRefValue(inputRef).click();
+        }}
+      >
+        <Image
+          src="/icons/plus-24.svg"
+          width={24}
+          height={24}
+          alt="아바타 업로드 버튼"
+        />
+      </button>
+    </div>
+  );
 };
 
 export default AvatarUploader;
