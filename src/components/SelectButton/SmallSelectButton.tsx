d51644e8e166779ca03941edf5c5e2c2ee0d0624
wherehows <sensitive.hu@gmail.com>
2024-02-18 21:10:53 +0900

    feat(SelectButton): 보더가 없는 경우도 추가

--- src/components/SelectButton/SmallSelectButton.tsx
+++ src/components/SelectButton/SmallSelectButton.tsx
@@ -3,18 +3,26 @@ import { classNames } from "@/utils/helpers";
 import { ReactNode } from "react";
 
 interface Props {
+  withBorder?: boolean;
   Icon: ReactNode;
   text: string;
   placeholder: string;
   onClick: EmptyFunction;
 }
 
-const SmallSelectButton = ({ Icon, text, placeholder, onClick }: Props) => {
+const SmallSelectButton = ({
+  withBorder = true,
+  Icon,
+  text,
+  placeholder,
+  onClick,
+}: Props) => {
   return (
     <button
       onClick={onClick}
       className={classNames(
-        "flex justify-between items-center border-solid border-[1px] border-grey-02 rounded-[20px] text-button4",
+        "flex justify-between items-center rounded-[20px] text-button4",
+        withBorder && "border-solid border-[1px] border-grey-02",
         text && "bg-skyblue-01text-white",
         "h-[28px] pl-[8px] pr-[6px]"
       )}
