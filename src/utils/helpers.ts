70f3889ccc24da81585b3aa9de031a5633a82dfd
wherehows <sensitive.hu@gmail.com>
2024-02-28 16:47:06 +0900

    chore(helpers): generateRandomId 함수 추가

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -8,3 +8,15 @@ export const getKeys = Object.keys as <T extends object>(
 ) => Array<keyof T>;
 
 export const getRefValue = <C>(ref: RefObject<C>) => ref.current as C;
+
+export const generateRandomId = (length: number) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let randomId = "";
+  for (let i = 0; i < length; i++) {
+    randomId += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return randomId;
+};
