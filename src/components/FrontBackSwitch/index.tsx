6581c91365b243aba69d33945f0e3f3385ee63fd
wherehows <sensitive.hu@gmail.com>
2024-02-28 16:50:58 +0900

    chore(FrontBackSwitch): 리펙토링

--- src/components/FrontBackSwitch/index.tsx
+++ src/components/FrontBackSwitch/index.tsx
@@ -1,26 +1,29 @@
 "use client";
 
+import { EmptyFunction } from "@/types/common";
 import { classNames } from "@/utils/helpers";
-import { useState } from "react";
 
-const FrontBackSwitch = () => {
-  const [isFrontSide, setIsFrontSide] = useState(true);
+interface Props {
+  isFront: boolean;
+  onClickSwitch: EmptyFunction;
+}
 
+const FrontBackSwitch = ({ isFront, onClickSwitch }: Props) => {
   return (
     <div className="flex relative w-fit rounded-[16px] bg-grey-01">
       <div
         className={classNames(
           "absolute top-0 w-[57px] h-[32px] bg-skyblue-01 rounded-[16px] duration-500",
-          isFrontSide ? "left-0" : "left-[56px]"
+          isFront ? "left-0" : "left-[56px]"
         )}
       />
       <button
         className={classNames(
           "relative pointer bg-transparent pl-[16px] pr-[16px] h-[32px] flex justify-center items-center",
-          isFrontSide ? "text-button3 text-white" : "text-button4 text-grey-04"
+          isFront ? "text-button3 text-white" : "text-button4 text-grey-04"
         )}
         onClick={() => {
-          !isFrontSide && setIsFrontSide(true);
+          onClickSwitch();
         }}
       >
         앞면
@@ -28,10 +31,10 @@ const FrontBackSwitch = () => {
       <button
         className={classNames(
           "relative pointer bg-transparent pl-[16px] pr-[16px] h-[32px] flex justify-center items-center",
-          isFrontSide ? "text-button4 text-grey-04" : "text-button3 text-white"
+          isFront ? "text-button4 text-grey-04" : "text-button3 text-white"
         )}
         onClick={() => {
-          isFrontSide && setIsFrontSide(false);
+          onClickSwitch();
         }}
       >
         뒷면
