d9a70d85105a5fda6e4ef12411fc2ba63b3f6a22
wherehows <sensitive.hu@gmail.com>
2024-06-03 17:34:26 +0900

    chore: remove unused file

--- src/app/create/route/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div>개발중</div>;
-}
--- src/app/mypage/likets/[idx]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-
-export default function Page() {
-  return <div>개발중</div>;
-}
