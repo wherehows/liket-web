57a96b685eb51c513258048636f43fac11b9a7ca
wherehows <sensitive.hu@gmail.com>
2024-06-16 19:19:41 +0900

    feat: change text size and padding value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,225 +26,227 @@ export default function Home() {
         <Header.LeftOption logo />
         <Header.RightOption option={{ search: true, like: true }} />
       </Header>
-      <Carousel imgs={CAROUSEL_DUMMY} />
-      <section className="pl-[24px] mb-[48px] mt-[24px]">
-        <h2 className="mb-[8px]">
-          선선한 가을 날씨에 <span className="text-skyblue-01">#힐링</span>하기
-          좋은 곳 🍁
-        </h2>
-        <ScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%] cursor-grab touch-action-none">
-          {CONTENT_CARDS_DUMMY.map((data, index) => {
-            return <ContentCard key={index} {...data} />;
-          })}
-        </ScrollContainer>
-      </section>
-      <section className="pl-[24px]">
-        <h2 className="mb-[8px]">
-          요즘 <span className="text-skyblue-01">#10대</span> Z세대가 주목하는
-          곳 ✨
-        </h2>
-        <ScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%]">
-          {CONTENT_CARDS_DUMMY.map((data, index) => {
-            return <ContentCard key={index} {...data} />;
-          })}
-        </ScrollContainer>
-      </section>
-      <Divider height="8px" width="100%" margin="24px 0" />
-      <section className="pl-[24px]">
-        <div className="flex flex-row mb-[8px]">
-          <h2>핫플차트</h2>
-          <div className="text-body5 text-grey-04 flex flex-col-reverse ml-[8px]">{`업로드 Date`}</div>
-        </div>
-        <ScrollContainer className="flex flex-row overflow-x-hidden gap-[8px] overflow-y-hidden w-[100%]">
-          <div>
-            <Link
-              href="/category?type=팝업스토어&orderby=famous"
-              className="flex item-center"
-            >
-              <div className="text-skyblue-01 text-body4">팝업 스토어</div>
-              <RightArrow
-                fill={colors.skyblue["01"]}
-                style={{
-                  display: "inline",
-                }}
-              />
-            </Link>
-            <ul>
-              {HOT_PLACE_DUMMY_1.map((dummy, index) => {
-                return (
-                  <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
-                    <div className="text-numbering1 mr-[18px] center align-middle">
-                      {index + 1}
-                    </div>
-                    <HotPlaceItem {...dummy} />
-                  </li>
-                );
-              })}
-            </ul>
+      <main>
+        <Carousel imgs={CAROUSEL_DUMMY} />
+        <section className="mb-[48px] mt-[24px]">
+          <h2 className="pl-[24px] mb-[8px]">
+            선선한 가을 날씨에 <span className="text-skyblue-01">#힐링</span>
+            하기 좋은 곳 🍁
+          </h2>
+          <ScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%] cursor-grab touch-action-none [&>*:last-child]:mr-[24px] [&>*:first-child]:ml-[24px]">
+            {CONTENT_CARDS_DUMMY.map((data, index) => {
+              return <ContentCard key={index} {...data} />;
+            })}
+          </ScrollContainer>
+        </section>
+        <section>
+          <h2 className="pl-[24px] mb-[8px]">
+            요즘 <span className="text-skyblue-01">#10대</span> Z세대가 주목하는
+            곳 ✨
+          </h2>
+          <ScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%] [&>*:last-child]:mr-[24px] [&>*:first-child]:ml-[24px]">
+            {CONTENT_CARDS_DUMMY.map((data, index) => {
+              return <ContentCard key={index} {...data} />;
+            })}
+          </ScrollContainer>
+        </section>
+        <Divider height="8px" width="100%" margin="24px 0" />
+        <section>
+          <div className="pl-[24px] flex flex-row mb-[8px]">
+            <h2>핫플차트</h2>
+            <div className="text-body5 text-grey-04 flex flex-col-reverse ml-[8px]">{`업로드 Date`}</div>
           </div>
-          <div>
-            <Link
-              href="/category?type=전시회&orderby=famous"
-              className="flex item-center"
-            >
-              <div className="text-skyblue-01 text-body4">전시회</div>
-              <RightArrow
-                fill={colors.skyblue["01"]}
-                style={{
-                  display: "inline",
-                }}
-              />
-            </Link>
-            <ul>
-              {HOT_PLACE_DUMMY_2.map((dummy, index) => {
-                const { idx, thumbnail } = dummy;
-                return (
-                  <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
-                    <div className="text-numbering1 mr-[18px] center align-middle">
-                      {index + 1}
-                    </div>
-                    <HotPlaceItem {...dummy} thumbnail={thumbnail + idx} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div>
-            <Link
-              href="/category?type=연극&orderby=famous"
-              className="flex item-center"
-            >
-              <div className="text-skyblue-01 text-body4">연극</div>
-              <RightArrow
-                fill={colors.skyblue["01"]}
-                style={{
-                  display: "inline",
-                }}
-              />
-            </Link>
-            <ul>
-              {HOT_PLACE_DUMMY_3.map((dummy, index) => {
-                const { idx, thumbnail } = dummy;
-                return (
-                  <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
-                    <div className="text-numbering1 mr-[18px] center align-middle">
-                      {index + 1}
-                    </div>
-                    <HotPlaceItem {...dummy} thumbnail={thumbnail + idx} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div>
-            <Link
-              href="/category?type=뮤지컬&orderby=famous"
-              className="flex item-center"
-            >
-              <div className="text-skyblue-01 text-body4">뮤지컬</div>
-              <RightArrow
-                fill={colors.skyblue["01"]}
-                style={{
-                  display: "inline",
-                }}
-              />
-            </Link>
-            <ul>
-              {HOT_PLACE_DUMMY_4.map((dummy, index) => {
-                const { idx, thumbnail } = dummy;
-                return (
-                  <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
-                    <div className="text-numbering1 mr-[18px] center align-middle">
-                      {index + 1}
-                    </div>
-                    <HotPlaceItem {...dummy} thumbnail={thumbnail + idx} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div>
-            <Link
-              href="/category?type=콘서트&orderby=famous"
-              className="flex item-center"
-            >
-              <div className="text-skyblue-01 text-body4">콘서트</div>
-              <RightArrow
-                fill={colors.skyblue["01"]}
-                style={{
-                  display: "inline",
-                }}
-              />
-            </Link>
-            <ul>
-              {HOT_PLACE_DUMMY_5.map((dummy, index) => {
-                const { idx, thumbnail } = dummy;
-                return (
-                  <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
-                    <div className="text-numbering1 mr-[18px] center align-middle">
-                      {index + 1}
-                    </div>
-                    <HotPlaceItem {...dummy} thumbnail={thumbnail + idx} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div>
-            <Link
-              href="/category?type=페스티벌&orderby=famous"
-              className="flex item-center"
-            >
-              <div className="text-skyblue-01 text-body4">페스티벌</div>
-              <RightArrow
-                fill={colors.skyblue["01"]}
-                style={{
-                  display: "inline",
-                }}
-              />
-            </Link>
-            <ul>
-              {HOT_PLACE_DUMMY_6.map((dummy, index) => {
-                const { idx, thumbnail } = dummy;
-                return (
-                  <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
-                    <div className="text-numbering1 mr-[18px] center align-middle">
-                      {index + 1}
-                    </div>
-                    <HotPlaceItem {...dummy} thumbnail={thumbnail + idx} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </ScrollContainer>
-      </section>
-      <Divider height="8px" width="100%" margin="24px 0" />
-      <section className="pl-[24px] mb-[48px]">
-        <h2 className="mb-[8px]">오픈 예정 컨텐츠</h2>
-        <ScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%]">
-          {CONTENT_CARDS_DUMMY.map((data, index) => {
-            return <ContentCard key={index} {...data} />;
-          })}
-        </ScrollContainer>
-      </section>
-      <section className="pl-[24px]">
-        <h2 className="mb-[8px]">종료 예정 컨텐츠</h2>
-        <ScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%]">
-          {CONTENT_CARDS_DUMMY.map((data, index) => {
-            return <ContentCard key={index} {...data} />;
-          })}
-        </ScrollContainer>
-      </section>
-      <Divider height="8px" width="100%" margin="24px 0" />
-      <section className="pl-[24px] mb-[24px]">
-        <h2 className="mb-[8px]">최근 인기 리뷰</h2>
-        <ScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%]">
-          {REVIEW_CARDS_DUMMY.map((data, index) => {
-            return <ReviewCard key={index} {...data} />;
-          })}
-        </ScrollContainer>
-      </section>
+          <ScrollContainer className="flex flex-row overflow-x-hidden gap-[8px] overflow-y-hidden w-[100%] [&>*:last-child]:mr-[24px] [&>*:first-child]:ml-[24px]">
+            <div>
+              <Link
+                href="/category?type=팝업스토어&orderby=famous"
+                className="flex item-center"
+              >
+                <div className="text-skyblue-01 text-body4">팝업 스토어</div>
+                <RightArrow
+                  fill={colors.skyblue["01"]}
+                  style={{
+                    display: "inline",
+                  }}
+                />
+              </Link>
+              <ul>
+                {HOT_PLACE_DUMMY_1.map((dummy, index) => {
+                  return (
+                    <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
+                      <div className="text-numbering1 mr-[18px] center align-middle">
+                        {index + 1}
+                      </div>
+                      <HotPlaceItem {...dummy} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div>
+              <Link
+                href="/category?type=전시회&orderby=famous"
+                className="flex item-center"
+              >
+                <div className="text-skyblue-01 text-body4">전시회</div>
+                <RightArrow
+                  fill={colors.skyblue["01"]}
+                  style={{
+                    display: "inline",
+                  }}
+                />
+              </Link>
+              <ul>
+                {HOT_PLACE_DUMMY_2.map((dummy, index) => {
+                  const { idx, thumbnail } = dummy;
+                  return (
+                    <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
+                      <div className="text-numbering1 mr-[18px] center align-middle">
+                        {index + 1}
+                      </div>
+                      <HotPlaceItem {...dummy} thumbnail={thumbnail + idx} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div>
+              <Link
+                href="/category?type=연극&orderby=famous"
+                className="flex item-center"
+              >
+                <div className="text-skyblue-01 text-body4">연극</div>
+                <RightArrow
+                  fill={colors.skyblue["01"]}
+                  style={{
+                    display: "inline",
+                  }}
+                />
+              </Link>
+              <ul>
+                {HOT_PLACE_DUMMY_3.map((dummy, index) => {
+                  const { idx, thumbnail } = dummy;
+                  return (
+                    <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
+                      <div className="text-numbering1 mr-[18px] center align-middle">
+                        {index + 1}
+                      </div>
+                      <HotPlaceItem {...dummy} thumbnail={thumbnail + idx} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div>
+              <Link
+                href="/category?type=뮤지컬&orderby=famous"
+                className="flex item-center"
+              >
+                <div className="text-skyblue-01 text-body4">뮤지컬</div>
+                <RightArrow
+                  fill={colors.skyblue["01"]}
+                  style={{
+                    display: "inline",
+                  }}
+                />
+              </Link>
+              <ul>
+                {HOT_PLACE_DUMMY_4.map((dummy, index) => {
+                  const { idx, thumbnail } = dummy;
+                  return (
+                    <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
+                      <div className="text-numbering1 mr-[18px] center align-middle">
+                        {index + 1}
+                      </div>
+                      <HotPlaceItem {...dummy} thumbnail={thumbnail + idx} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div>
+              <Link
+                href="/category?type=콘서트&orderby=famous"
+                className="flex item-center"
+              >
+                <div className="text-skyblue-01 text-body4">콘서트</div>
+                <RightArrow
+                  fill={colors.skyblue["01"]}
+                  style={{
+                    display: "inline",
+                  }}
+                />
+              </Link>
+              <ul>
+                {HOT_PLACE_DUMMY_5.map((dummy, index) => {
+                  const { idx, thumbnail } = dummy;
+                  return (
+                    <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
+                      <div className="text-numbering1 mr-[18px] center align-middle">
+                        {index + 1}
+                      </div>
+                      <HotPlaceItem {...dummy} thumbnail={thumbnail + idx} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div>
+              <Link
+                href="/category?type=페스티벌&orderby=famous"
+                className="flex item-center"
+              >
+                <div className="text-skyblue-01 text-body4">페스티벌</div>
+                <RightArrow
+                  fill={colors.skyblue["01"]}
+                  style={{
+                    display: "inline",
+                  }}
+                />
+              </Link>
+              <ul>
+                {HOT_PLACE_DUMMY_6.map((dummy, index) => {
+                  const { idx, thumbnail } = dummy;
+                  return (
+                    <li className="flex my-[13px] w-[256px]" key={dummy.idx}>
+                      <div className="text-numbering1 mr-[18px] center align-middle">
+                        {index + 1}
+                      </div>
+                      <HotPlaceItem {...dummy} thumbnail={thumbnail + idx} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </ScrollContainer>
+        </section>
+        <Divider height="8px" width="100%" margin="24px 0" />
+        <section className="mb-[48px]">
+          <h2 className="pl-[24px] mb-[8px]">오픈예정 컨텐츠</h2>
+          <ScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%] [&>*:last-child]:mr-[24px] [&>*:first-child]:ml-[24px]">
+            {CONTENT_CARDS_DUMMY.map((data, index) => {
+              return <ContentCard key={index} {...data} />;
+            })}
+          </ScrollContainer>
+        </section>
+        <section>
+          <h2 className="pl-[24px] mb-[8px]">종료예정 컨텐츠</h2>
+          <ScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%] [&>*:last-child]:mr-[24px] [&>*:first-child]:ml-[24px]">
+            {CONTENT_CARDS_DUMMY.map((data, index) => {
+              return <ContentCard key={index} {...data} />;
+            })}
+          </ScrollContainer>
+        </section>
+        <Divider height="8px" width="100%" margin="24px 0" />
+        <section className="mb-[24px]">
+          <h2 className="pl-[24px] mb-[8px]">최근 인기 리뷰</h2>
+          <ScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%] [&>*:last-child]:mr-[24px] [&>*:first-child]:ml-[24px]">
+            {REVIEW_CARDS_DUMMY.map((data, index) => {
+              return <ReviewCard key={index} {...data} />;
+            })}
+          </ScrollContainer>
+        </section>
+      </main>
       <LinkableTab shadow />
     </>
   );
