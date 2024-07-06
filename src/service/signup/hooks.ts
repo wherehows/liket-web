f39c5b3d5f94726d3720913e0a168e734ef2b47c
wherehows <sensitive.hu@gmail.com>
2024-07-06 21:28:11 +0900

    chore: make service for signup

--- src/service/signup/hooks.ts
+++ src/service/signup/hooks.ts
@@ -1,10 +1,25 @@
 import { UseMutationOptions, useMutation } from "@tanstack/react-query";
 import queryOptions from "./options";
-import { CheckAuthenticationParam, SendAuthenticationParam } from "./model";
+import {
+  CheckAuthenticationParam,
+  CheckAuthenticationResponse,
+  SendAuthenticationParam,
+  SignupParam,
+} from "./model";
 
 export const useSendAuthentication = (
   props: UseMutationOptions<unknown, Error, SendAuthenticationParam, unknown>
 ) => useMutation({ ...queryOptions.send(), ...props });
+
 export const useCheckAuthentication = (
-  props: UseMutationOptions<unknown, Error, CheckAuthenticationParam, unknown>
+  props: UseMutationOptions<
+    CheckAuthenticationResponse,
+    Error,
+    CheckAuthenticationParam,
+    unknown
+  >
 ) => useMutation({ ...queryOptions.check(), ...props });
+
+export const useSignup = (
+  props: UseMutationOptions<unknown, Error, SignupParam, unknown>
+) => useMutation({ ...queryOptions.signup(), ...props });
