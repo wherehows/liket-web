2ac4ce2bdb8c4fe4e5c4ec6b09d3672f9297dfd2
wherehows <sensitive.hu@gmail.com>
2024-07-09 11:24:17 +0900

    feat: make password change page

--- src/app/find/password/page.tsx
+++ src/app/find/password/page.tsx
@@ -4,12 +4,28 @@ import Header from "@/components/Header";
 import { useRouter } from "next/navigation";
 import EmailForm from "@/components/SignupForm/EmailForm";
 import { useState } from "react";
+import PasswordForm from "@/components/SignupForm/PasswordForm";
+import { useResetPassword } from "@/service/reset/hooks";
 
 export default function Page() {
   const [formIndex, setFormIndex] = useState(0);
+  const [emailToken, setEmailToken] = useState("");
 
   const router = useRouter();
-  const updateForm = () => setFormIndex(formIndex + 1);
+  const updateForm = ({ emailToken }: { emailToken: string }) => {
+    setFormIndex(formIndex + 1);
+    setEmailToken(emailToken);
+  };
+  const { mutate } = useResetPassword({
+    onSuccess: () => {
+      router.replace("/login/email");
+    },
+  });
+  const handleClickNextButtonInPasswordForm = (pw: string) =>
+    mutate({
+      pw,
+      emailToken,
+    });
 
   return (
     <>
@@ -24,6 +40,13 @@ export default function Page() {
         <Header.MiddleText text="비밀번호 재설정" />
       </Header>
       {formIndex === 0 && <EmailForm updateForm={updateForm} />}
+      {formIndex === 1 && (
+        <PasswordForm
+          isResetForm
+          nextButtonText="변경하기"
+          onClickNextButton={handleClickNextButtonInPasswordForm}
+        />
+      )}
     </>
   );
 }
