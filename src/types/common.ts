0857fbf829c82d1a265d6a9aa5e4321ba040825b
wherehows <sensitive.hu@gmail.com>
2024-07-12 17:17:43 +0900

    chore: change typeof leftoption in header

--- src/types/common.ts
+++ src/types/common.ts
@@ -12,14 +12,12 @@ export type XOR<T, U> = T | U extends object
   ? (Without<T, U> & U) | (Without<U, T> & T)
   : T | U;
 
-export type IconButtonOption =
-  | boolean
-  | {
-      disabled?: boolean;
-      active?: boolean;
-      color?: string;
-      onClick?: EmptyFunction;
-    };
+export type IconButtonOption = {
+  disabled?: boolean;
+  active?: boolean;
+  color?: string;
+  onClick?: EmptyFunction;
+};
 
 export type PartialPick<T, F extends keyof T> = Omit<T, F> &
   Partial<Pick<T, F>>;
