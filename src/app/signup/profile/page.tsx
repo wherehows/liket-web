825e16778b085c531942163dec31faa0eb99836c
wherehows <sensitive.hu@gmail.com>
2024-07-12 22:27:46 +0900

    feat: add social singup logic

--- src/app/signup/profile/page.tsx
+++ src/app/signup/profile/page.tsx
@@ -7,9 +7,10 @@ import Header from "@/components/Header";
 import EmailForm from "@/components/SignupForm/EmailForm";
 import PasswordForm from "@/components/SignupForm/PasswordForm";
 import ProfileForm from "@/components/SignupForm/ProfileForm";
-import { useLocalSignup } from "@/service/signup/hooks";
+import { useSocialSignup } from "@/service/signup/hooks";
 import { ProfileFormData } from "@/types/signup";
-import { useRouter } from "next/navigation";
+import customToast from "@/utils/customToast";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 
 const INITIAL_FORM_STATE = {
@@ -24,6 +25,10 @@ const INITIAL_FORM_STATE = {
 
 const SignUpPage = () => {
   const router = useRouter();
+  const params = useSearchParams();
+
+  // TODO: 잘못된 접근 처리 필요.
+  const token = params.get("token") as string;
   const [formInformation, setFormInformation] = useState(INITIAL_FORM_STATE);
   const [formIndex, setFormIndex] = useState(0);
   const updateForm = (insertedFormData: Partial<typeof INITIAL_FORM_STATE>) => {
@@ -31,9 +36,25 @@ const SignUpPage = () => {
     setFormIndex(formIndex + 1);
   };
 
-  const { mutate } = useLocalSignup({
-    onSuccess: (res) => {
-      console.log(res);
+  const { mutate } = useSocialSignup({
+    onSuccess: () => {
+      router.push("/");
+    },
+    onError: ({ response }) => {
+      if (response?.status === 400) {
+        customToast("잘못된 폼 데이터가 전달됐습니다.");
+        return;
+      }
+
+      if (response?.status === 401) {
+        customToast("토큰 정보가 잘못됐습니다.");
+        return;
+      }
+
+      if (response?.status === 409) {
+        customToast("이메일 혹은 닉네임이 중복됩니다.");
+        return;
+      }
     },
   });
 
@@ -47,7 +68,7 @@ const SignUpPage = () => {
     gender,
   }: ProfileFormData) => {
     mutate({
-      ...formInformation,
+      token,
       file,
       nickname,
       birth: birth,
