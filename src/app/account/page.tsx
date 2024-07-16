46ed20fee2d12e52a72da00d8374f20271808a4e
wherehows <sensitive.hu@gmail.com>
2024-07-16 22:27:45 +0900

    feat: add logout feature

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -6,9 +6,17 @@ import LeftOption from "@/components/Header/LeftOption";
 import MiddleText from "@/components/Header/MiddleText";
 import LinkItem from "@/components/LinkItem";
 import KaKao from "@/icons/kakao.svg";
+import { useLogout } from "@/service/login/hooks";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+  const { mutate } = useLogout({
+    onSuccess: () => router.replace("/"),
+  });
+  const handleClickLogout = () => mutate();
+
   return (
     <>
       <Header>
@@ -25,7 +33,10 @@ export default function Page() {
         </div>
         <Divider width="100%" height="8px" margin="16px 0 0 0" />
         <LinkItem href="/mypage/edit/password">비밀번호 변경</LinkItem>
-        <button className="text-h2 w-[100%] h-[48px] flex items-center px-[24px]">
+        <button
+          className="text-h2 w-[100%] h-[48px] flex items-center px-[24px]"
+          onClick={handleClickLogout}
+        >
           로그아웃
         </button>
         <Link
